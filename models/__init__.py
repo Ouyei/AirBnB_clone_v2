614b8bd97ad328bcecd6411efd99e6b016a26cee
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-04-11 15:52:14 -0300

    init_update

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
+"""This module instantiates an storage"""
 from os import getenv
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
-if HBNB_TYPE_STORAGE == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
