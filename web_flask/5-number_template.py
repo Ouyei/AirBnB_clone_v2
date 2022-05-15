b0da32c53d89744f2926044574f8453c077d1fdf
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-05-15 21:50:26 +0000

    task5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,66 +2,39 @@
 """Starts a Flask web application
 """
 from flask import Flask, render_template
+
 app = Flask(__name__)
-app.strict_slashes = False
 
 
-@app.route('/')
-def hello_flask():
-    """
-    Return desired string
-    """
+@app.route('/', strict_slashes=False)
+def index():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Return desired string for /hbnb route
-    """
     return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """
-    Return desired string for /c/<text> route, replace _ with space
-    """
-    return "C {}".format(text.replace("_", " "))
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    return "C %s" % text.replace("_", " ")
 
 
-@app.route('/python/<text>')
-def python_is_magic(text):
-    """
-    Return desired string for /python/<text> route, replace _ with space
-    """
-    return "Python {}".format(text.replace("_", " "))
-
-
+@app.route('/python/<string:text>')
 @app.route('/python/')
-def python_is_cool():
-    """
-    Return default string for /python/ route
-    """
-    text = "is cool"
-    return "Python {}".format(text)
+def python_text(text="is cool"):
+    return "Python %s" % text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>')
-def is_it_a_number(n):
-    """
-    Return a string only if valid int
-    """
-    return "{} is a number".format(n)
+def n_num(n):
+    return "%d is a number" % n
 
 
 @app.route('/number_template/<int:n>')
-def number_template(n):
-    """
-    Displays an html page only if number is int
-    """
-    return render_template('5-number.html', number=n)
+def num_template(n):
+    return render_template('5-number.html', value=n)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
