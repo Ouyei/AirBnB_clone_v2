6140d9948608a95cec4b0e730a901c8a0a13400e
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-05-15 21:53:36 +0000

    task5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,35 +6,45 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
+@app.route("/", strict_slashes=False)
+def hello_AirBnB():
+    """Displays Hello HBNB!"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Displays HBNB"""
     return "HBNB"
 
 
-@app.route('/c/<string:text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
-    return "C %s" % text.replace("_", " ")
+    """ Display C follow by the value of text"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python/<string:text>')
-@app.route('/python/')
-def python_text(text="is cool"):
-    return "Python %s" % text.replace('_', ' ')
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonn(text="is cool"):
+    """Display default= is cool, other case = Python + <text>"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
-@app.route('/number/<int:n>')
-def n_num(n):
-    return "%d is a number" % n
+@app.route("/number/<int:n>", strict_slashes=False)
+def integ(n):
+    """Display "<n> is a number" only if <n> is a integer"""
+    if type(n) == int:
+        return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>')
-def num_template(n):
-    return render_template('5-number.html', value=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_html(n):
+    """Display HTML and change h1 page only if n is an integer"""
+    return render_template("5-number.html", num=n)
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
